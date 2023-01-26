63429f45182b06e5f7c487969d0aeaf6b929dea4
OgelGames <olliverdc28@gmail.com>
2023-01-27 07:54:13 +1100

    Code improvements and bug fixes (#2)
    * fix global variable and luacheck
    
    * code improvements and fixes
    
    * let there be tabs
    
    * redo the changes i made...

--- api.lua
+++ api.lua
@@ -1,52 +1,67 @@
---global for api
-pick_axe_tweaks = {}
+
+function pick_axe_tweaks.is_light_node(name)
+	local def = minetest.registered_nodes[name]
+	if def and def.paramtype == "light" and def.light_source and def.light_source > 0 then
+		return true
+	end
+	return false
+end
 
 local function default_light_node()
-    local node = minetest.settings:get("pat_light_node")
+	local node = minetest.settings:get("pat_light_node")
 
-    if not minetest.registered_nodes[node] then
-        return "default:torch"
-    elseif minetest.registered_nodes[node].paramtype ~= "light" then
-        minetest.log("error", node .. " is does not have a paramtype of light")
-    else
-        return node
-    end
+	if not minetest.registered_nodes[node] then
+		return "default:torch"
+	elseif not pick_axe_tweaks.is_light_node(node) then
+		minetest.log("error", "[pick_axe_tweaks] Node " .. node .. " is not a light")
+	else
+		return node
+	end
+end
+
+local function place_light(itemstack, player, pointed_thing)
+	-- Should prevent crashes from mods passing fake players improperly
+	if type(player) ~= "userdata" or pointed_thing.type ~= "node" then
+		return
+	end
+
+	local node = minetest.get_node(pointed_thing.under)
+	local def = minetest.registered_nodes[node.name]
+	if def and def.on_rightclick and not player:get_player_control().sneak then
+		return def.on_rightclick(pointed_thing.under, node, player, itemstack, pointed_thing)
+	end
+
+	local pmeta = player:get_meta()
+	if pmeta:get_string("pa_status") == "false" then
+		return
+	end
+	local light_node = default_light_node()
+	if pmeta:get_string("pa_node") ~= "" then
+		light_node = pmeta:get_string("pa_node")
+	end
+	if not pick_axe_tweaks.is_light_node(light_node) then
+		return
+	end
+
+	node = minetest.get_node(pointed_thing.above)
+	def = minetest.registered_nodes[node.name]
+	if not def or not def.buildable_to or not def.drawtype == "airlike" then
+		return
+	end
+
+	local pinv = player:get_inventory()
+	if pinv:contains_item("main", light_node) then
+		minetest.registered_nodes[light_node].on_place(ItemStack(light_node), player, pointed_thing)
+		pinv:remove_item("main", light_node)
+	end
 end
 
 function pick_axe_tweaks.register_pick_axes(pick_axes)
-    for _, pa in pairs(pick_axes) do
-        if minetest.registered_items[pa] then
-            minetest.override_item(pa, {
-                on_place = function(itemstack, player, pointed_thing)
-                    --should prevent crashes of mods passing fake players improperly
-                    if type(player) ~= "userdata" then return end
-                    local pmeta = player:get_meta()
-                    if pmeta:get_string("pa_status") == "false" then
-                        return
-                    end
-
-                    local light_node = default_light_node()--"default:torch"
-                    if pmeta:get_string("pa_node") ~= "" then light_node = pmeta:get_string("pa_node") end
-                    local pinv = player:get_inventory()
-                    local contains_ln = pinv:contains_item("main", light_node)
-                    local is_air = minetest.get_node(pointed_thing.above).name == "air"
-                    local noddef = minetest.registered_nodes[minetest.get_node(pointed_thing.under).name]
-
-                    if not player:get_player_control().sneak and noddef.on_rightclick then
-                        return minetest.item_place(itemstack, player, pointed_thing)
-                    elseif pointed_thing.type ~= "node" then return
-                    end
-
-                    if contains_ln and is_air then
-                        if minetest.registered_nodes[light_node].paramtype2 == "wallmounted" then
-                            minetest.registered_nodes[light_node].on_place(itemstack, player, pointed_thing)
-                        else
-                            minetest.item_place(ItemStack(light_node), player, pointed_thing)
-                        end
-                        pinv:remove_item("main", light_node)
-                    end
-                end
-            })
-        end
-    end
+	for _, pa in pairs(pick_axes) do
+		if minetest.registered_items[pa] then
+			minetest.override_item(pa, {
+				on_place = place_light,
+			})
+		end
+	end
 end
