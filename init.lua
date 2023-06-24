4af0bf307f730113903a6a0bc593a968519eab29
OgelGames <olliverdc28@gmail.com>
2023-06-24 15:29:14 +1000

    use correct API function for registering pickaxes
    should have been fixed in 6c56b4a (which is when the API was changed)

--- init.lua
+++ init.lua
@@ -8,11 +8,9 @@ dofile(modpath .. "/chat_commands.lua")
 
 -- Register all tools with `pickaxe` group
 minetest.register_on_mods_loaded(function()
-	local pickaxes = {}
 	for name, def in pairs(minetest.registered_items) do
 		if def.type == "tool" and def.groups and def.groups.pickaxe then
-			table.insert(pickaxes, name)
+			pick_axe_tweaks.register_tool(name)
 		end
 	end
-	pick_axe_tweaks.register_pick_axes(pickaxes)
 end)
